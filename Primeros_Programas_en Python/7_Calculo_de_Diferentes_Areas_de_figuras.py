4664c650e907a1fecde792b40ec2af62c9ef8cfa
= <=>
2024-02-22 12:00:39 +0100

    Cambios necesarios

--- Primeros_Programas_en Python/7_Calculo_de_Diferentes_Areas_de_figuras.py
+++ Primeros_Programas_en Python/7_Calculo_de_Diferentes_Areas_de_figuras.py
@@ -10,21 +10,26 @@ eleccion =int(input("""Elija la figura que quieres calcular el area:
 def Cuadrado():
     lado=float(input("Ingrese el tamaño de su lado: "))
     area=lado*lado
-    print(f"El area es{area}")
+    area = round(area, 2)
+    print(f"El area es {area}")
 def Triangulo():
     base=float(input("Ingrese la longitud de la base: "))
     altura=float(input("Ingrese la longitud de la altura: "))
     area=base*altura
+    area = round(area, 2)
     print(f"El area es {area}")
 def Circulo():
     radio=float(input("Ingrese el radio del circulo: "))
     numeroPi= math.pi #Para poner el numero Pi se hace de esta forma
     area=math.pi*(radio**2)
+    area = round(area, 2)
     print(f"El area es {area}")
 def Rombo():
     diagonalMayor=float(input("Ingrese su diagonal mayor: "))
     diagonalMenor=float(input("Ingrese su diagonal menor: "))
     area=(diagonalMayor*diagonalMenor)/2
+    area=round(area,2)
+    print(f"El area es {area}")
 def default():
     print("Opcion no valida")
 menu = {
